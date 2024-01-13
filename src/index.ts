835bbfddba7f9162aaa1a3e6afc79369573ae370
PhilLavoie <PhilLavoie@users.noreply.github.com>
2024-01-13 18:04:49 +0000

    CONFLICTS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { getInputs, runActionHandler } from "@infra-blocks/github-actions";
 import { handler } from "./handler.js";
+<<<<<<< HEAD
 import { parseInputs } from "./inputs.js";
 
 runActionHandler(() => {
   return handler(parseInputs(getInputs("exactly-once", "pull-request")));
+=======
+import { parseInputs } from "./intputs.js";
+
+runActionHandler(() => {
+  return handler(parseInputs(getInputs("example-input")));
+>>>>>>> template/master
 });
